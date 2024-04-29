43ea419e34dc7122da7e66312a4f68ed4db0aa15
melodie <melodie.bernard123@yahoo.fr>
2024-04-29 19:22:33 +0200

    changed plugins

--- nvim/lua/custom/plugins/c_formatter_42.lua
+++ nvim/lua/custom/plugins/c_formatter_42.lua
@@ -1,7 +1,7 @@
-require('c_formatter_42')
-
-c_formatter_42 {
-vim.g.c_formatter_42_clang_format = '/usr/bin/clang-format' -- Chemin vers clang-format
-vim.g.c_formatter_42_autoformat = 1 -- Activer le formatage automatique
-vim.g.c_formatter_42_format_on_save = 1 -- Formater le code à la sauvegarde
+-- File: ~/.config/nvim/lua/custom/plugins/c_formatter_42.lua
+return {
+ 'cacharle/c_formatter_42.vim',
+ config = function()
+ end
 }
+
