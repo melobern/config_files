2d7d37c3aba9af609ca4b62dd0ab5a8234299d1c
melodie <melodie.bernard123@yahoo.fr>
2024-04-29 19:38:12 +0200

    plugins neovim

--- nvim/lua/custom/plugins/c_formatter_42.lua
+++ nvim/lua/custom/plugins/c_formatter_42.lua
@@ -1,7 +1,5 @@
 -- File: ~/.config/nvim/lua/custom/plugins/c_formatter_42.lua
 return {
- 'cacharle/c_formatter_42.vim',
- config = function()
- end
+  'cacharle/c_formatter_42.vim',
+  config = function() end,
 }
-
