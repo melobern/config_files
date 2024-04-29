8cf2b83c39dc13f3abcb2650869cf11ecb36ff57
melodie <melodie.bernard123@yahoo.fr>
2024-04-29 19:22:33 +0200

    changed plugins

---
+++ nvim/lua/custom/plugins/42header.lua
@@ -0,0 +1,8 @@
+-- File: ~/.config/nvim/lua/custom/plugins/42header.lua
+return {
+ '42Paris/42header',
+ config = function()
+  vim.g.user42 = 'mbernard'
+  vim.g.mail42 = 'mbernard@student.42lyon.fr'
+ end
+}
